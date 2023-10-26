1dee3d625acdf42527cff91400bc071d65e15daf
Karl Phillips <k@rlphilli.ps>
2023-10-26 03:37:46 +0000

    create force-checked prop for button

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './styles.css';
 
 export default function Button(props) {
-  const { onClick, label } = props;
+  const { onClick, label, forceActive } = props;
 
   return (
     <button
       type="button"
-      className="Button"
+      className={`Button ${forceActive ? 'forceActive' : ''}`}
       onClick={onClick}
       aria-label={label}
     >
