6baf76ba722a16a08489ce3d2c674d00ba3fadcf
Karl Phillips <k@rlphilli.ps>
2023-10-31 00:22:53 +0000

    hack a quick backdrop into the modal

--- src/Modal/index.jsx
+++ src/Modal/index.jsx
@@ -4,11 +4,18 @@ import Button from '../Button';
 
 function Modal({ isOpen, onCancel, onConfirm }) {
   return (
-    <div className={`modal ${isOpen ? 'open' : ''}`}>
-      <h2>Clear Deck?</h2>
-      <Button onClick={onConfirm} label="Yes" />
-      <Button onClick={onCancel} label="CANCEL" />
-    </div>
+    <>
+      <div className={`modal ${isOpen ? 'open' : ''}`}>
+        <h2>Clear Deck?</h2>
+        <Button onClick={onConfirm} label="Yes" />
+        <Button onClick={onCancel} label="CANCEL" />
+      </div>
+      <div
+        className={`modalBackdrop ${isOpen ? 'open' : ''}`}
+        onClick={onCancel}
+        aria-hidden
+      />
+    </>
   );
 }
 
