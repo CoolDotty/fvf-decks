c157ec2a21fc289833ad1443368f716195728bc8
Karl Phillips <k@rlphilli.ps>
2023-11-22 02:28:53 +0000

    Add Sable Santana BFF Update

--- src/const.js
+++ src/const.js
@@ -9,6 +9,7 @@ export const defaultCardSort = (a, b) => (a.batch === b.batch ? a.id - b.id : a.
 // 4 = Baba's Laundromat
 // 5 = Wired Wrecks
 // 6 = Halloween Update (2023)
+// 7 = Sable Santana BFF
 
 const Card = (img, batch, type, name, cost) => ({
   id: parseInt(img.substring(5, 9), 10),
@@ -118,6 +119,7 @@ export const allCards = [
   Card('Card_0113_bake.png', 4, 'Buff', 'Painkillers', 3),
   Card('Card_1004_bake.png', 0, 'Personality', 'Badass', 2),
   Card('Card_0143_bake.png', 6, 'Helper', 'Bat Turret Lover', 2),
+  Card('Card_0140_bake.png', 7, 'Helper', 'Laser Fence', 2),
 ].sort(defaultCardSort);
 
 export const cards = allCards.filter(({ type }) => type !== 'Personality');
