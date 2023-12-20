8190f5af26172cf668f6a5ba6cbe54e10bfc6e9a
Dot <k@rlphilli.ps>
2023-12-20 03:02:43 +0000

    Create holiday cheer!

---
+++ src/HolidayFX/index.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useRef, useMemo } from 'react';
+import './styles.css';
+
+// Snow comes from: https://codepen.io/tutsplus/pen/BaVqjvg
+
+const date = new Date();
+// months are 0 indexed
+const december1 = new Date(date.getFullYear(), 11, 1);
+const december25 = new Date(date.getFullYear(), 11, 25);
+
+const snowContent = [<>&#10052;</>, <>&#10053;</>, <>&#10054;</>];
+
+const random = (num) => Math.floor(Math.random() * num);
+
+function HolidayFX() {
+  const snowContainer = useRef();
+  const [isSnowing] = useState(date >= december1 && date <= december25);
+
+  const flakes = useMemo(() => [...Array(100)].map((_, i) => (
+    <div
+      // eslint-disable-next-line react/no-array-index-key
+      key={i}
+      className="snow"
+      style={{
+        top: `-${random(100)}%`,
+        left: `${random(100)}%`,
+        fontSize: `${random(10) + 10}px`,
+        animationDuration: `${random(25) + 25}s`,
+      }}
+    >
+      {snowContent[random(3)]}
+    </div>
+  )), []);
+
+  return (
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {isSnowing ? (
+        <div ref={snowContainer} id="snowContainer">
+          {flakes}
+        </div>
+      ) : null}
+    </>
+  );
+}
+
+export default HolidayFX;
--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
+import HolidayFX from './HolidayFX';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="*" Component={App} />
+        <Route
+          path="*"
+          Component={(props) => (
+            <>
+              <HolidayFX />
+              <App {...props} />
+            </>
+          )}
+        />
       </Routes>
     </Router>
   </React.StrictMode>,
